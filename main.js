7e93c1ec5afd7f1db0e9983de1117d070ed6a2f6
martinpvz <martin.paniagua200013@gmail.com>
2022-10-09 18:13:57 -0500

    algo

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hola")
+
+function voltearCarta(elemento) {
+    elemento.style.display = "none";
+    console.log(elemento)
+    elemento.nextSibling.nextElementSibling.style.display = "block"
+}
--- main.py
+++ main.py
@@ -1,1 +1,71 @@
-print("Hola")
+#!/usr/bin/env python3
+
+# HTML is following
+print('Content-Type: text/html')
+
+# Leave a blank line
+print('')
+
+
+print("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memorama</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="header">
+        <img src="./img/casino.png" alt="Casino">
+    </div>
+    <div class="main">
+        <div class="tablero">
+            <div class="carta" id="carta1">
+                <img src="./img/back.png" alt="carta" class="back" id="back1" onclick="voltearCarta(this)">
+                <img src="./img/alonso.png" class="front">
+            </div>
+            <div class="carta" id="carta2">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/bottas.png" class="front">
+            </div>
+            <div class="carta" id="carta3">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/checo.png" class="front">
+            </div>
+            <div class="carta" id="carta4">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/gasly.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta5">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/hamilton.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta6">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/checo.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta7">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/gasly.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta8">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/bottas.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta9">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/alonso.png" alt="" class="front">
+            </div>
+            <div class="carta" id="carta10">
+                <img src="./img/back.png" alt="carta" class="back" onclick="voltearCarta(this)">
+                <img src="./img/hamilton.png" alt="" class="front">
+            </div>
+        </div>
+    </div>
+    <script src="./main.js"></script>
+</body>
+</html>
+""")
